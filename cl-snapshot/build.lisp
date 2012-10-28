9b435ab9db33ecaf511d2b965a348b5d19171b37
Christoph Finkensiep <ch.finkensiep@freenet.de>
2012-10-28 18:40:39 +0200

    modified the lisp build script to use core compression if available

--- cl-snapshot/build.lisp
+++ cl-snapshot/build.lisp
@@ -1,5 +1,12 @@
 (ql:quickload "cl-snapshot")
+#-sb-core-compression
 (sb-ext:save-lisp-and-die "snap"
 			  :toplevel #'snap::sb-main
 			  :executable t
 			  :purify t)
+#+sb-core-compression
+(sb-ext:save-lisp-and-die "snap"
+			  :toplevel #'snap::sb-main
+			  :executable t
+			  :purify t
+			  :compression t)
